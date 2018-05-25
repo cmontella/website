ae6d451f9daa03341246267cf8e671c7b843b03b
cmontella <cmontella@live.com>
2018-05-25 12:15:21 -0700

    Switch out site

---
+++ js/toggle.js
@@ -0,0 +1,33 @@
+    function toggle(id) 
+    {
+        var state = document.getElementById(id).style.display;
+        if (state == 'block') 
+	{
+       		document.getElementById(id).style.display = 'none';
+       	} 
+	else 
+	{
+        	document.getElementById(id).style.display = 'block';
+        }
+     }
+/*
+     FILE ARCHIVED ON 07:07:04 Mar 08, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:57:47 May 25, 2018.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 808.423 (3)
+  esindex: 0.01
+  captures_list: 919.055
+  CDXLines.iter: 11.778 (3)
+  PetaboxLoader3.datanode: 725.017 (6)
+  exclusion.robots: 0.646
+  exclusion.robots.policy: 0.618
+  RedisCDXSource: 93.858
+  PetaboxLoader3.resolve: 624.153 (3)
+  load_resource: 564.795
+*/
